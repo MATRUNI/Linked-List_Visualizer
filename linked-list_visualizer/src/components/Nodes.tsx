0f3d2fafba0ad7d0e28f45696342d7e16b7abcde
Vandana6261 <vvandanapatidar992@gmail.com>
2026-03-08 17:41:54 +0530

    feat:- drag and drop without useRef

---
+++ linked-list_visualizer/src/components/Nodes.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+type node = {
+    x: number,
+    y: number,
+    data: string
+}
+function Nodes() {
+
+    const nodes: node[] = [
+        {x: 200, y: 150, data: "Node A"},
+        {x: 200, y: 150, data: "Node B"},
+        {x: 200, y: 150, data: "Node C"},
+    ]
+
+    const [isDragging, setIsDragging] = useState(false);
+    const [position, setPosition] = useState({x: 200, y: 150})
+    const [offset, setOffset] = useState({x: 0, y: 0})
+
+    const mouseDownHandler = (e:React.MouseEvent<HTMLDivElement>) => {
+        console.log("Down called")
+        setIsDragging(true)
+        setOffset({
+            x: e.clientX - position.x,
+            y: e.clientY - position.y,
+        })
+    }
+    const mouseUpHandler = () => {
+        console.log("up called")
+        setIsDragging(false)
+    }
+    const mouseMoveHandler = (e:React.MouseEvent<HTMLDivElement>) => {
+        if(isDragging) {
+            setPosition({
+                x: e.clientX - offset.x,
+                y: e.clientY - offset.y,
+            });
+        }
+    }
+  return (
+    <>
+        <div
+            onMouseUp={mouseUpHandler}
+            onMouseDown={mouseDownHandler}
+            onMouseMove={mouseMoveHandler}
+            style={{
+                userSelect: "none",
+                cursor: 'pointer',
+                position: "absolute",
+                left:`${position.x}px`,
+                top:`${position.y}px`,
+            }}
+        >
+            <span>
+                {nodes[0].data}
+            </span>
+        </div>
+    </>
+  ) 
+}
+
+export default Nodes
