6b99496e531d445da9f91cadd09b6357a488dd54
Vandana6261 <vvandanapatidar992@gmail.com>
2026-03-10 00:17:47 +0530

    wip:- make line between 2 nodes

--- linked-list_visualizer/src/components/Canvas.tsx
+++ linked-list_visualizer/src/components/Canvas.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Nodes from "./Nodes";
-import './Canva.css';
+import './Canvas.css'
+
 function Canvas() {
     const [nodes, setNodes] = useState([
-        { id: "1", x: 400, y: 250, data: "Node A", next: "2"},
-        { id: "2", x: 100, y: 130, data: "Node B", next: '3'},
-        { id: "3", x: 200, y: 50, data: "Node C", next: null },
+        { id: "1", x: 100, y: 200, data: "Node A", next: "2"},
+        { id: "2", x: 200, y: 200, data: "Node B", next: '3'},
+        { id: "3", x: 300, y: 200, data: "Node C", next: "4" },
+        { id: "4", x: 400, y: 200, data: "Node C", next: null },
     ]);
 
     const updatePosition = (id: string, x: number, y: number) => {
@@ -22,18 +24,31 @@ function Canvas() {
         className="canvas"
       >
         <svg>
-            <line 
-                x1={nodes[0].x + 25}
-                y1={nodes[0].y + 15}
-                x2={nodes[1].x + 25}
-                y2={nodes[1].y + 15}
-                stroke="white"
-                strokeWidth={"2"}
-            />
+          {nodes.map((node, i) => {
+            if(!node.next) return null;
+
+            const target = nodes[i+1]
+
+            return (
+
+              <line 
+                  x1={node.x + 25}
+                  y1={node.y + 15}
+                  x2={target.x + 25}
+                  y2={target.y + 15}
+                  stroke="whitesmoke"
+                  strokeWidth={"2"}
+              />
+            )
+          })}
         </svg>
 
         {nodes.map(node => (
-            <Nodes key={node.id} {...node} onDrag={(id, x, y) => updatePosition(id, x, y)} />
+            <Nodes 
+              key={node.id} 
+              {...node} 
+              onDrag={(id, x, y) => updatePosition(id, x, y)} 
+            />
         ))}
       </div>
     </>
---
+++ linked-list_visualizer/src/components/DrawLine.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function DrawLine() {
+  return (
+    <>
+      <svg>
+        <line 
+            x1={x}
+            y1={y}
+        />
+      </svg>
+    </>
+  )
+}
+
+export default DrawLine
