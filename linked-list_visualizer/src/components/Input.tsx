c166992f83b3f399835dcfcfbe7f8fe54c8d3f47
Vandana6261 <vvandanapatidar992@gmail.com>
2026-03-10 15:31:50 +0530

    feat: input (wip)

--- linked-list_visualizer/src/components/Input.tsx
+++ linked-list_visualizer/src/components/Input.tsx
@@ -3,8 +3,16 @@ import './Input.css'
 
 function Input() {
     const [input, setInput] = useState("")
+    const [error, setError] = useState("");
+
+    const regex = /^(\d+(?:[ ,]\d+)*|[A-Z]+(?:[ ,][A-Z]+)*|[a-z]+(?:[ ,][a-z]+)*)$/
 
     const handleSubmit = (e:React.SubmitEvent) => {
+        if(!input) {
+            setError("Please provide some value first")
+            return;
+        }
+        // regex.test(input)
         e.preventDefault();
         const arr = input.split(/[\s,]+/).map(val => val)
         console.log(arr)
@@ -18,6 +26,7 @@ function Input() {
             value={input}
             onChange={(e) => setInput(e.target.value)}
         />
+        {error && <p className='error'>{error}</p>}
         <button className='btn'>Submit</button>
       </form>
     </>
