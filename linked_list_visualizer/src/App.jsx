29d3faf7ed28fcf61cfa18e2723a711be53f6aa6
MATRUNI <h.uyadav2004@gmail.com>
2026-03-17 23:24:45 +0530

    TSX to JSX (Connections)

--- linked_list_visualizer/src/App.jsx
+++ linked_list_visualizer/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-
+import Canvas from './components/Canvas'
+import Input from './components/Input'
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div id='app-div'>
+      <Canvas/>
+      <Input />
+    </div>
   )
 }
 
